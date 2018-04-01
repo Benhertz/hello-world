dd74dffe5baef20d94ab5720913951540f271443
Ben Hertzberg <37953286+Benhertz@users.noreply.github.com>
2018-04-01 16:25:26 -0700

    Mario.c
    simple stair builder

---
+++ Mario.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <cs50.h>
+
+
+int main()
+{
+    int height = -1;
+    int line = 0;
+    int counter = 0;
+
+    while (height < 0 || height > 23)
+    {
+        //printf("Enter the height: ");
+        height = get_int("Enter the height: ");
+    }
+    printf("\n");
+
+    for(line = 1; line <= height; line++)
+    {
+        int spaceNum = height - line;
+        int spaceCounter = 1;
+        for (spaceCounter = 0; spaceCounter < spaceNum ; spaceCounter++)
+            printf(" ");
+        for(counter = 0; counter <= line ; counter++)
+            printf("#");
+
+        printf("\n");
+    }
+
+
+    return 0;
+}
